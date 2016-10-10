3e9ee5ad4ff97748153c99ee864bda53bbd7168f
Chuck Dulde <chuck@sell-on.com>
2016-10-10 18:27:15 +0000

    Final Refactor

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-$(document).ready(function() {
-	//FizzBuzz Challenge
-	for ( var i = 1; i <= 100; i++ ) {
-		console.log(i);
-		if ( i % 15 == 0 ) {
-			console.log("fizz buzz");
-			$("body").append("fizz buzz");
-			$("body").append("<br/>");
-		}
-		else if ( i % 3 == 0 ) {
-			console.log("fizz");
-			$("body").append("fizz");
-			$("body").append("<br/>");
-		}
-		else if ( i % 5 == 0 ) {
-			console.log("buzz");
-			$("body").append("buzz");
-			$("body").append("<br/>");
-		} else {
-			console.log(i);
-			$("body").append(i);
-			$("body").append("<br/>");
+	function fizzBuzz(number) {
+		for ( var i = 1; i <= number; i++ ) {
+			if ( i % 15 == 0 ) {
+				$("body").append("fizz buzz");
+				$("body").append("<br/>");
+			}
+			else if ( i % 3 == 0 ) {
+				$("body").append("fizz");
+				$("body").append("<br/>");
+			}
+			else if ( i % 5 == 0 ) {
+				$("body").append("buzz");
+				$("body").append("<br/>");
+			} 
+			else {
+				$("body").append(i);
+				$("body").append("<br/>");
+			}
 		}
 	}
+	
+$(document).ready(function() {
+	//FizzBuzz Refactor
+	
+	//Once Page is Loaded, Prompt for Integer Input
+	var input = prompt("Enter an INTEGER to FizzBuzz","e.g. 15");
+	input = parseInt(+input);
+	
+	// Call fizzBuzz function
+	fizzBuzz(input);
+
 });
